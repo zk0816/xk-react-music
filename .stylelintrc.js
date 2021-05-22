c3af6783a8e291110b0e7bae3b3b795567864e54
zhangkang <zhangkang@feewee.cn>
2021-05-22 17:25:05 +0800

    头部左边图片

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,12 @@ module.exports = {
   extends: "stylelint-config-standard",
   rules: {
     // your rules
+    "rule-empty-line-before": [
+          "always-multi-line",
+          {
+            "except": ["first-nested", "inside-block"]
+          }
+        ],
   },
   // 忽略其他文件，只校验样式相关的文件
   ignoreFiles: [
--- src/components/MusicHeader/index.tsx
+++ src/components/MusicHeader/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import style from "./index.less";
 
 const MusicHeader = () => {
   return (
-    <div style={{ display: 'flex', textAlign: 'center' }}>
-      <NavLink to="/discover">发现音乐111</NavLink>
-      <NavLink to="/mine">我的音乐</NavLink>
-      <NavLink to="/friend">朋友</NavLink>
+    <div className={style.m_top}>
+      <div style={{display: 'flex'}}>
+        <div className={style.top_img}></div>
+        <div className={style.top_title}>
+          <NavLink to="/discover">发现音乐</NavLink>
+          <NavLink to="/mine">我的音乐</NavLink>
+          <NavLink to="/friend">朋友</NavLink>
+          <NavLink to="/one">商城</NavLink>
+          <NavLink to="/two">音乐人</NavLink>
+          <NavLink to="/three">下载客户端</NavLink>
+        </div>
+      </div>
+      <div>666</div>
     </div>
   );
 };
